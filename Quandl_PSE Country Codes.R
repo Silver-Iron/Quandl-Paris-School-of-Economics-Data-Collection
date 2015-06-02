7bbf06c4bf19a504f1da4f6677a5ee62b247edf0
Kevin McGovern <kmcg82004@gmail.com>
2015-06-02 16:12:18 -0400

    R-Quandl Data Collection Working

--- Quandl_PSE Country Codes.R
+++ Quandl_PSE Country Codes.R
@@ -2,7 +2,7 @@ setwd("~/GitHub/Quandl Paris School of Economics Data Collection")
 library(Quandl)
 
 #authenticate Quandl
-Quandl.auth("")
+Quandl.auth("PLACE_YOUR_TOKEN_HERE") #Insert your API key before running
 
 #get country names and codes into data.frame
 CountryCodes <- read.csv('Quandl_PSE Country Codes.csv')
@@ -10,23 +10,39 @@ CountryCodes <- read.csv('Quandl_PSE Country Codes.csv')
 #get indicator for all data sets in Paris School of Econmics set
 Indicators <- read.csv('Quandl_PSE Indicators.csv')
 
+#store search indicator
+searchIndicator <- Indicators$Indicator.Code[80]  #6210301
+  
 #initialize variable for final data.frame
 final_PSEData<-NULL
 
-for (i in 1:length(CountryCodes$Code)){
 #loop through country codes
+for (i in 1:length(CountryCodes$Code)){
+  #create search string
+  searchString <- paste("PSE/",CountryCodes$Code[i],"_",searchIndicator,sep="")
+  
+  tryCatch({
+    #for each country code, store data set in temporary variable
+    myPSEData <- Quandl(searchString)
+    
+    #add country name and code to temp variable
+    myPSEData$CountryCode <- CountryCodes$Code[i]
+    myPSEData$Country <- CountryCodes$Country[i]
+    
+    #append to final data.frame
+    final_PSEData <- rbind(final_PSEData,myPSEData)
+    },
+    warning = function(war){
+      print(paste("MY_WARNING:  ",war))
+      print(searchString)
+    },
+    error = function(err){
+      print(paste("MY_ERROR:  ",err))
+      print(searchString)
+    }
+  )#end tryCatch
 
-  #for each country code, store data set in temporary variable
+}#end for loop
 
-  #add country name and code to temp variable
-
-  #append to final data.frame
-  final_PSEData <- rbind(final_PSEData,myPSEData)
-
-}
-
-myPSEData <- Quandl("PSE/ARG_6210301")
-
-myPSEData$CountryCode <- 'ARG'
-
-myPSEData$Country <- 'Argentina'
+#write data to csv
+write.csv(final_PSEData, file="final_PSE_1PctCountries.csv")
